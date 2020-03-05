32b662cfad39825cd437a9f86b222918a1327a1d
dogcolley <jinji0326@naver.com>
2020-03-05 23:07:05 +0900

    피시 작업 틀 완료

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             <ul className="J_list01 PT_ht_P85 T_ov_at U_bg_cfff">
               <li className="T_ps_rl T_pd_Pall5 T_ds_block U_bd_btm01">
                 <a href="#" className="T_ds_block "> 
-                  <p class="T_pd_Pht5">여기에는 글의 앞부분 2줄이 2줄이 들어갑니다.</p>
+                  <p className="T_pd_Pht5">여기에는 글의 앞부분 2줄이 2줄이 들어갑니다.</p>
                   <div className="T_fl_Crt clear T_mg_CPlt2">
                     <button type="button" className="" onClick="">좋아요</button>
                     <button type="button" className="" onClick="">싫어요</button>
@@ -35,7 +35,7 @@ function App() {
               </li>
             </ul>
             <form onSubmit="" id="" className="PT_ht_P10 T_fl_Clt T_pd_Ptop6">
-              <input class="IM_bd_all0 T_wd_P80 T_ht_full" name="" value="" id=""/>
+              <input className="IM_bd_all0 T_wd_P80 T_ht_full" name="" value="" id=""/>
               <button className="T_wd_P20 T_ht_full">글작성하기</button>
             </form>
             <div className="T_ps_ab clear T_wd_full T_pd_Pwd5" style={{top:2+'%',left:0}}>
@@ -46,16 +46,33 @@ function App() {
             </div>
           </article>
 
-          <article id="j_comment" className="T_ds_non">
-            <h3>여기에는 뎃글들이 들어갑니다.</h3>
-            <ul>
-              <li></li>
+
+          <article id="j_comment" className="T_ps_ab PC_wd_P40 T_ht_full PT_pd_Pwd2 T_pd_Pht4 U_bg_cf9 T_ps_rl">
+            <h3 className="PT_ft_sz25 T_ft_wh700 U_ft_c000 PT_ht_P5">Commend</h3>
+            <ul className="J_list01 PT_ht_P85 T_ov_at U_bg_cfff">
+              <li className="T_ps_rl T_pd_Pall5 T_ds_block U_bd_btm01">
+                <a href="#" className="T_ds_block "> 
+                  <p className="T_pd_Pht5">여기에는 뎃글들이 들어갑니다.</p>
+                  <div className="T_fl_Crt clear T_mg_CPlt2">
+                    <button type="button" className="" onClick="">좋아요</button>
+                    <button type="button" className="" onClick="">싫어요</button>
+                    <button type="button" className="" onClick="">커멘드</button>
+                  </div>
+                </a>
+              </li>
             </ul>
-            <form>
-            <input/>
-            <button>댓글입력하기</button>
+            <form onSubmit="" id="" className="PT_ht_P10 T_fl_Clt T_pd_Ptop6">
+              <input className="IM_bd_all0 T_wd_P80 T_ht_full" name="" value="" id=""/>
+              <button className="T_wd_P20 T_ht_full">댓글입력</button>
             </form>
+            <div className="T_ps_ab clear T_wd_full T_pd_Pwd5" style={{top:2+'%',left:0}}>
+              <button className="T_mg_Prt2">새로고침</button>
+              <button className="T_mg_Prt2">닉로그인</button>
+              <button>신고하기</button>
+              <button className="T_fl_rt">야간테마</button>
+            </div>
           </article>
+
 
           <article id="J_best" className="PT_fl_lt T_ht_full PC_wd_P30 PT_pd_Pwd1 T_pd_Pht4 U_bg_c000 T_ps_rl U_bd_right01">
             <h3 className="PT_ft_sz25 T_ft_wh700 U_ft_cfff PT_ht_P5">여기엔 베스트글이</h3>
@@ -63,7 +80,7 @@ function App() {
             <li className="T_ps_rl T_pd_Pall5 T_ds_block U_bd_btm01">
                 <div className="T_ps_rl">
                   <h4>익명A <span className="sound_only">작성자</span></h4>
-                  <div class="T_ps_ab" style={{right:0,top:0}}>
+                  <div className="T_ps_ab" style={{right:0,top:0}}>
                     <button>삭제</button>
                     <button>공유하기</button>
                     <button>신고하기</button>
@@ -84,7 +101,7 @@ function App() {
               <li className="T_ps_rl T_pd_Pall5 T_ds_block U_bd_btm01">
                 <div className="T_ps_rl">
                   <h4>익명A <span className="sound_only">작성자</span></h4>
-                  <div class="T_ps_ab" style={{right:0,top:0}}>
+                  <div className="T_ps_ab" style={{right:0,top:0}}>
                     <button>삭제</button>
                     <button>공유하기</button>
                     <button>신고하기</button>
@@ -101,10 +118,20 @@ function App() {
           </article>
         </section>
       
-        <div className="T_ds_non">
-          여기부터는 팝업차이 생성되는 공간입니다.
-          [삭제 묻기 및 패스워드 입력문구, 신고 팝업, 로그인 팝업등이 여기에 자리잡습니다.]
+        <div id="popLogin" className="T_ps_ab T_wd_full T_ht_full T_ds_table" style={{background:'rgba(0,0,0,0.7)',top:0,left:0}}>
+          <button type="button" className="T_ps_ab T_wd_full T_ht_full" style={{top:0,left:0}}>닫기</button>
+          <div className="U_bg_cfff T_pd_Pwd2 T_pd_Pht6 T_wd_P40 T_ht_P55 T_ps_ab" style={{margin:'auto',minWidth:'300px',left:0,right:0,top:0,bottom:0}}>
+            
+            <h3 className="T_mg_Pbtm5 T_ft_ct PT_ft_sz35 U_ft_c000">여기에 팝업 제목</h3>
+            <form>
+              <input className="T_mg_Pbtm3 U_bd_all01 T_wd_full T_pd_Pht3 T_pd_Pwd4" />
+              <input className="U_bd_all01 T_wd_full T_mg_Pbtm8 T_pd_Pht3 T_pd_Pwd4" />
+              <button type="button" className="T_wd_full T_pd_Pht3 T_mg_Pbtm2 U_bg_c000">보내기</button>
+              <button type="button" className="T_wd_full T_pd_Pht3 U_bd_all01">닫기</button>
+            </form>
+          </div>
         </div>
+
       </div>
     </>
   );
